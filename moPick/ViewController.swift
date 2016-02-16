522f56793b909698a0c5e3c42427539e2c016abf
dicrotect <dicrotect9050@gmail.com>
2016-02-16 15:53:26 +0800

    Initial Commit

---
+++ moPick/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  moPick
+//
+//  Created by dicrotect on 2016/02/16.
+//  Copyright © 2016年 kensuke takakura. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
