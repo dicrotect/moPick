f574ed2d47e1996818ad5dc4343c3d5ac189efe0
dicrotect <dicrotect9050@gmail.com>
2016-02-18 13:34:39 +0800

    画面遷移/レイアウト/ユーザデフォルトの作成

---
+++ moPick/swipeMovieViewController.swift
@@ -0,0 +1,35 @@
+//
+//  swipeMovieViewController.swift
+//  moPick
+//
+//  Created by dicrotect on 2016/02/18.
+//  Copyright © 2016年 kensuke takakura. All rights reserved.
+//
+
+import UIKit
+
+class swipeMovieViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
